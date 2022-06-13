5688dd8ac072f0f77b1276a6e3470b02fce6a81e
badjr13 <dorrance.bobby@gmail.com>
2022-06-13 09:24:26 -0400

    Start migrating to clap builder pattern
    I find it much easier to read what is happening while using the
    builder pattern with clap.

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,77 @@
-use clap::{Parser, Subcommand};
-
-mod board;
-use crate::board::BoardCommand;
-
-mod ticket;
-use crate::ticket::TicketCommand;
+use clap::{Arg, Command};
 
 fn main() {
-    let args = GrangerArguments::parse();
-    parse(args)
-}
-
-#[derive(Debug, Parser)]
-#[clap(about, author, version)]
-struct GrangerArguments {
-    #[clap(subcommand)]
-    subcommands: GrangerSubcommand,
-}
-
-#[derive(Debug, Subcommand)]
-#[clap(disable_help_subcommand = true)]
-enum GrangerSubcommand {
-    /// Manage Boards
-    Board(BoardCommand),
-
-    /// Manage Tickets
-    Ticket(TicketCommand),
-}
-
-fn parse(args: GrangerArguments) {
-    match &args.subcommands {
-        GrangerSubcommand::Board(option) => {
-            if option.init {
-                println!("INIT")
-            }
-            if option.list {
-                println!("LIST")
-            }
-            if option.remove {
-                println!("REMOVE")
-            }
-        }
-        GrangerSubcommand::Ticket(option) => {
-            if option.create {
-                println!("CREATE")
-            }
-            if option.read.is_some() {
-                println!("READ")
-            }
-            if option.update.is_some() {
-                println!("UPDATE")
-            }
-            if option.delete.is_some() {
-                println!("DELETE")
-            }
-            if option.list {
-                println!("LIST")
-            }
-            if option.r#move.is_some() {
-                println!("MOVE")
-            }
-        }
-    }
+    let matches = Command::new("granger")
+        .about("An opinionated Kanban Board for the solo developer.")
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("board")
+                .about("Manage Boards")
+                .arg(
+                    Arg::new("init")
+                        .short('i')
+                        .long("init")
+                        .help("Initialize a board in local git repository"),
+                )
+                .arg(
+                    Arg::new("list")
+                        .short('l')
+                        .long("list")
+                        .help("List all boards on system"),
+                )
+                .arg(
+                    Arg::new("remove")
+                        .short('r')
+                        .long("remove")
+                        .help("Remove board based on local git repository"),
+                ),
+        )
+        .subcommand(
+            Command::new("ticket")
+                .about("Manage Tickets")
+                .arg(
+                    Arg::new("create")
+                        .short('c')
+                        .long("create")
+                        .help("Create a new ticket")
+                        .display_order(1),
+                )
+                .arg(
+                    Arg::new("read")
+                        .short('r')
+                        .long("read")
+                        .help("View details of an existing ticket")
+                        .display_order(2),
+                )
+                .arg(
+                    Arg::new("update")
+                        .short('u')
+                        .long("update")
+                        .help("Update details of an existing ticket")
+                        .display_order(3),
+                )
+                .arg(
+                    Arg::new("delete")
+                        .short('d')
+                        .long("delete")
+                        .help("Delete an existing ticket")
+                        .display_order(4),
+                )
+                .arg(
+                    Arg::new("list")
+                        .short('l')
+                        .long("list")
+                        .help("List all existing tickets in local board")
+                        .display_order(5),
+                )
+                .arg(
+                    Arg::new("move")
+                        .short('m')
+                        .long("move")
+                        .help("Move ticket to a different state in local board")
+                        .display_order(6),
+                ),
+        )
+        .get_matches();
 }
