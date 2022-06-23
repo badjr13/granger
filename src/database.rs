cd32e6e58b9eeb5761ff715282af9667aeecbc7e
badjr13 <dorrance.bobby@gmail.com>
2022-06-23 09:08:19 -0400

    Singular database location

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,32 @@
 use rusqlite::{Connection, Result};
+use std::fs;
+use std::fs::OpenOptions;
+use std::path::PathBuf;
+
+pub fn get_granger_db_directory() -> PathBuf {
+    dirs::config_dir()
+        .expect("Failed to find laydown config directory")
+        .join("granger")
+}
+
+pub fn get_path_to_db() -> PathBuf {
+    let granger_db_directory = get_granger_db_directory();
+    fs::create_dir(&granger_db_directory).ok();
+
+    let granger_db_file: PathBuf = granger_db_directory.join("granger.db");
+
+    OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open(&granger_db_file)
+        .expect("Failed to find laydown.ron file");
+
+    granger_db_file
+}
 
 pub fn get_connection() -> Result<Connection, rusqlite::Error> {
-    let path = "./granger.db";
-    Connection::open(&path)
+    Connection::open(&get_path_to_db())
 }
 
 pub fn inintialize_database() -> Result<()> {
