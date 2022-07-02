b345db37b166cb22cfd4dc485a23ed38a14318a4
badjr13 <dorrance.bobby@gmail.com>
2022-07-01 20:39:09 -0400

    Create Ticket and State models

---
+++ src/ticket/model.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+pub enum State {
+    ToDo,
+    Blocked,
+    Doing,
+    Review,
+    Done,
+    Urgent,
+}
+
+#[derive(Debug)]
+pub struct Ticket {
+    pub id: usize,
+    pub title: String,
+    pub description: String,
+    pub state: State,
+}
